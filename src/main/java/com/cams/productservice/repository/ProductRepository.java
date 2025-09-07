7cc48147328831a1d4e2f9d9fc11ed5e89c1790e
Aditya Das <adityadasrcpp@gmail.com>
2025-09-07 17:40:24 +0530

    Delete src directory

--- src/main/java/com/cams/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cams.productservice.repository;
-
-import com.cams.productservice.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
